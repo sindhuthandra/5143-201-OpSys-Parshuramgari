47f6234bd679d912894ae047b19e7524cfef556a
thirupathireddy-parshuramgari <thirupathi.idks126@gmail.com>
2017-03-21 20:13:50 -0500

    Create commands.py

---
+++ assignments/shell/cmds/commands.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from ls import ls
+from pwd import pwd
+from cat import cat
+from mv import mv
+from cp import cp
+from mkdir import mkdir
+from rmdir import rmdir
+from rm import rm
+from hd import hd
+from tail import tail
+from wc import wc
+from grep import grep
+from history import history
+from chgdir import chgdir
+from less import less
+from chmod import chmod
+from who import who
+from sort import sort
+from hiscmd import hiscmd
